d11de75fb4f218ad421e3a20e66ffd1862b84f2e
Reynaldo Layme <layme.reynaldo@icloud.com>
2020-04-10 19:31:51 -0300

    feat(main) add items for call api

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, StatusBar } from 'react-native';
+
+import api from './services/api';
 
 // Não possuem valor semântico (significado)
 // Não possuem estilização própia
@@ -10,11 +12,21 @@ import { View, Text, StyleSheet, StatusBar } from 'react-native';
 // Text: p, span, strong, h1, h2, h3
 
 export default function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  },[]);
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <View style={styles.container}>
-        <Text style={styles.tittle}>Hello GoStack</Text>
+        {repositories.map(repositorie => (
+          <Text key={repositorie.id} style={styles.repositorie}>{repositorie.title}</Text>
+        ))}
       </View>
     </>
   );
@@ -28,9 +40,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
 
-  tittle: {
+  repositorie: {
     color: '#fff',
-    fontSize: 32,
-    fontWeight: 'bold',
+    fontSize: 20,
   },
 });
