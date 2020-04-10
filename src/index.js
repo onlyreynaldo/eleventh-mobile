e188b1925d028fb8c5d9f375ce96845079e586d7
Reynaldo Layme <layme.reynaldo@icloud.com>
2020-04-10 19:48:12 -0300

    feat(main) add flatlist for scroll of data

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar } from 'react-native';
 
 import api from './services/api';
-
-// Não possuem valor semântico (significado)
-// Não possuem estilização própia
-// Todos componentes possuem por padrão "display: flex"
-// Não temos herancia de estilos
-
-// View: dev, footer, header, main, aside, section
-// Text: p, span, strong, h1, h2, h3
 
 export default function App() {
   const [repositories, setRepositories] = useState([]);
@@ -23,11 +15,16 @@ export default function App() {
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-      <View style={styles.container}>
-        {repositories.map(repositorie => (
-          <Text key={repositorie.id} style={styles.repositorie}>{repositorie.title}</Text>
-        ))}
-      </View>
+
+      <SafeAreaView style={styles.container}>
+        <FlatList 
+          data={repositories}
+          keyExtractor={repositorie => repositorie.id}
+          renderItem={({ item: repositorie }) => (
+            <Text style={styles.repositorie}>{repositorie.title}</Text>
+          )}
+        />
+      </SafeAreaView>
     </>
   );
 }
@@ -36,8 +33,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#7159c1',
-    justifyContent: 'center',
-    alignItems: 'center',
   },
 
   repositorie: {
